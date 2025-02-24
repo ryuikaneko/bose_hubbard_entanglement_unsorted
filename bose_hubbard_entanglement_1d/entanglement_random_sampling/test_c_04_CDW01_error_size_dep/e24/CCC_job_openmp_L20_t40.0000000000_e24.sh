27e95027b12bfbdaeb5bb687c4d0b11c7354d333
Ryui Kaneko <27846552+ryuikaneko@users.noreply.github.com>
2025-02-24 18:43:44 +0900

    add directory bose_hubbard_entanglement_1d

---
+++ bose_hubbard_entanglement_1d/entanglement_random_sampling/test_c_04_CDW01_error_size_dep/e24/CCC_job_openmp_L20_t40.0000000000_e24.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+#$ -cwd
+#$ -V -S /bin/bash
+#$ -N EE20
+#$ -pe smp 1
+#$ -q dp11.q
+##$ -q dp.q
+##$ -q dp2.q
+##$ -q dp3.q
+##$ -q dp4.q
+##$ -q dp5.q
+##$ -q dp6.q
+##$ -q dp7.q
+
+#----
+
+export OMP_NUM_THREADS=1
+#prog=../bin/a.out
+prog=../bin/main
+
+#----
+
+L=20
+t=40.0000000000
+r=$((RANDOM*RANDOM))
+s=$((RANDOM*RANDOM))
+e=24
+f=0
+#f=1 ## output histtotal
+
+echo "${L} ${t} ${r} ${s} ${f}"
+
+date
+${prog} \
+  -L ${L} \
+  -t ${t} \
+  -r ${r} \
+  -s ${s} \
+  -e ${e} \
+  -f ${f}
+date
+
+#----
---
+++ bose_hubbard_entanglement_1d/entanglement_random_sampling/test_c_04_CDW01_error_size_dep/e25/AAA.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+nmax=64
+#nmax=48
+
+for e in \
+25
+#12 13 14 15 16 17 18 19 20
+do
+
+for L in \
+16 20 24 28 32 36 40 44 48 52 56 60
+#16
+do
+
+for n in \
+${nmax}
+#`seq 1 $((nmax))`
+do
+  t=`echo ${L} ${n} ${nmax} | awk '{printf("%.10f",2.0*$1*$2/$3)}'`
+#  t=`echo ${L} ${n} ${nmax} | awk '{printf("%.10f",1.5*$1*$2/$3)}'`
+  echo ${L} ${n} ${t}
+  sed 's/^t=.*/t='${t}'/g' _CCC_job_openmp.sh | \
+  sed 's/^L=.*/L='${L}'/g' | \
+  sed 's/^e=.*/e='${e}'/g' | \
+  sed 's/^#$ -N EE/#$ -N EE'${L}'/g' \
+  > CCC_job_openmp_L${L}_t${t}_e${e}.sh
+#
+for rnd in \
+`seq 1 32`
+do
+  qsub CCC_job_openmp_L${L}_t${t}_e${e}.sh
+done
+
+done
+
+done
+
+done
