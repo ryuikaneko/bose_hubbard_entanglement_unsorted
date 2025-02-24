33cfc433b4620b671e9895151e8c875b52a5c2d4
Ryui Kaneko <27846552+ryuikaneko@users.noreply.github.com>
2025-02-24 18:47:53 +0900

    add directory bose_hubbard_entanglement_2d

---
+++ bose_hubbard_entanglement_2d_dat/matrix_permanent_2d/permanent_c/test_05/dat_2/6x4/BBB_Lx6_Ly4_i000125.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+#$ -cwd
+#$ -V -S /bin/bash
+#$ -N test_ee
+#$ -pe smp 1
+#$ -q dp2.q,dp3.q,dp4.q,dp5.q,dp6.q,dp7.q,dp8.q,dp9.q,dp10.q
+##$ -q dp.q
+##$ -q dp2.q
+##$ -q dp3.q
+##$ -q dp4.q
+##$ -q dp5.q
+##$ -q dp6.q
+##$ -q dp7.q
+##$ -q dp8.q
+##$ -q dp9.q
+##$ -q dp10.q
+
+#----
+
+date
+
+Lx=6
+Ly=4
+t=6.123724
+
+echo ${Lx} ${Ly} ${t}
+
+../bin/main -x ${Lx} -y ${Ly} -t ${t}
+
+date
---
+++ bose_hubbard_entanglement_2d_dat/matrix_permanent_2d/permanent_c/test_05/dat_2/6x6/AAA.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+Lx=6
+Ly=6
+
+template=_qsub_BBB_template.sh
+subprog=qsub
+#subprog=sbatch
+
+for i in \
+`seq 0 200`
+do
+  ipad=`printf "%06d" "${i}"`
+  t=`echo ${i} ${Lx} ${Ly} | awk '{printf "%.6f",0.01*$1*sqrt($2*$3)}'`
+  echo generate ${Lx} ${Ly} ${ipad} ${t}
+  cat ${template} | sed 's/@Lx@/'${Lx}'/g' | sed 's/@Ly@/'${Ly}'/g' | sed 's/@t@/'${t}'/g' > BBB_Lx${Lx}_Ly${Ly}_i${ipad}.sh
+done
+
+
+for i in \
+`seq 0 200`
+do
+  ipad=`printf "%06d" "${i}"`
+  t=`echo ${i} ${Lx} ${Ly} | awk '{printf "%.6f",0.01*$1*$2*$3}'`
+  echo submit ${Lx} ${Ly} ${ipad} ${t}
+  ${subprog} BBB_Lx${Lx}_Ly${Ly}_i${ipad}.sh
+done
