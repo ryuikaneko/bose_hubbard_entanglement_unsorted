6be6f080e56003e65427e79b9ccb6b5712c50883
Ryui Kaneko <27846552+ryuikaneko@users.noreply.github.com>
2025-02-24 18:58:38 +0900

    add bose_hubbard_entanglement_2d_dat

---
+++ bose_hubbard_entanglement_2d_dat/matrix_permanent_2d/permanent_c_rnd/test_02_adjustable_exponent_filename_with_rnd_seed/dat_1_CDW01_error_size_dep/e27/CCC_job_openmp_Lx8_Ly6_t13.85640646055101754541_e27.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+#$ -cwd
+#$ -V -S /bin/bash
+#$ -N EE8x6
+#$ -pe smp 1
+#$ -q dp2.q,dp3.q,dp4.q,dp5.q,dp6.q,dp7.q,dp8.q,dp9.q,dp10.q
+##$ -q dp11.q
+##$ -q dp.q
+##$ -q dp2.q
+##$ -q dp3.q
+##$ -q dp4.q
+##$ -q dp5.q
+##$ -q dp6.q
+##$ -q dp7.q
+
+#----
+
+export OMP_NUM_THREADS=1
+#prog=../bin/a.out
+prog=../bin/main
+
+#----
+
+Lx=8
+Ly=6
+t=13.85640646055101754541
+r=$((RANDOM*RANDOM))
+s=$((RANDOM*RANDOM))
+e=27
+f=0
+#f=1 ## output histtotal
+
+echo "${Lx} ${Ly} ${t} ${r} ${s} ${f}"
+
+date
+${prog} \
+  -x ${Lx} \
+  -y ${Ly} \
+  -t ${t} \
+  -r ${r} \
+  -s ${s} \
+  -e ${e} \
+  -f ${f}
+date
+
+#----
